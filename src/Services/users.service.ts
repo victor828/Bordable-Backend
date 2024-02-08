5634ea2c9bc2599066a2e31e5227bd63366f3ab6
Víctor Orozco <veom104@gmail.com>
2024-02-07 20:26:07 -0800

    ✨ [ADD] add clas server Users
    added the class service_controller to work the data of petition

---
+++ src/Services/users.service.ts
@@ -0,0 +1,26 @@
+import { consults_Users } from "../Consult/consults";
+import { user } from "../Models/users.models";
+
+class Users {
+  async newUser(data: user) {
+    // Updated type declaration
+    const result = await consults_Users.newUser(data);
+    return result ? { ok: true, data: result } : { ok: false, data: result };
+  }
+
+  async getUser(user_id: string) {
+    const result = await consults_Users.getUser(user_id);
+    return result ? { ok: true, data: result } : { ok: false, data: result };
+  }
+
+  async login(data: user) {
+    const result = await consults_Users.login(data);
+    return result ? { ok: true, data: result } : { ok: false, data: result };
+  }
+
+  async delete(userId: string) {
+    await consults_Users.deleteUser(userId);
+    return { ok: true, mesage: `User has Deleted` };
+  }
+}
+export const service_User = new Users();
