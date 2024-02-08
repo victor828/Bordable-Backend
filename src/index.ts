2e979b27ea903bdcceb1b36d6cebd2fc14700e28
Víctor Orozco <veom104@gmail.com>
2024-02-07 23:57:29 -0800

    🔨 [UPDATE] update middleware  to boardsRouter

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { usersRouter } from "./Routes/users.routes";
+import { boardsRouter } from "./Routes/boards.routes";
 require("dotenv").config();
 
 const app = express();
@@ -7,9 +8,9 @@ const PORT = process.env["PORT"] || 0;
 
 app.use(express.json());
 app.use("", usersRouter);
+app.use("", boardsRouter);
 
 const server = app.listen(PORT, () => {
   const { port } = server.address() as any;
   console.log(`Escuchando al puerto ${port}`);
 });
-
